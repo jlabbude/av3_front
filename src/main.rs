192c6adc4dca30ea783192d8ea7a52857fc48196
Lucas <jlabbude@hotmail.com>
2024-06-28 17:18:12 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
-use mapboxgl::{LngLat, Map, MapOptions};
+use futures::channel::oneshot;
+use log::*;
+use mapboxgl::{event, LngLat, Map, MapEventListener, MapOptions, QueryFeatureOptions};
+use serde::Serialize;
 use std::{cell::RefCell, rc::Rc};
 use yew::prelude::*;
 use yew::{use_effect_with_deps, use_mut_ref};
+
+struct Listener {
+    tx: Option<oneshot::Sender<()>>,
+}
+
+impl MapEventListener for Listener {
+
+    fn on_click(&mut self, map: Rc<Map>, e: event::MapMouseEvent) {
+        let lng_lat = e.lng_lat;
+
+        let url = format!(
+            "http://localhost:8080/data?lng={}&lat={}",
+            lng_lat.lng, lng_lat.lat
+        );
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let response = reqwest::get(&url).await.unwrap();
+            let data = response.text().await.unwrap();
+
+            web_sys::window()
+                .unwrap()
+                .document()
+                .unwrap()
+                .get_element_by_id("features")
+                .expect("Element \"features\" not found")
+                .set_inner_html(&data);
+        });
+    }
+
+    fn on_load(&mut self, _map: Rc<Map>, _e: event::MapBaseEvent) {
+        self.tx.take().unwrap().send(()).unwrap();
+    }
+
+
+}
 
 #[hook]
 fn use_map() -> Rc<RefCell<Option<Rc<Map>>>> {
@@ -11,11 +49,20 @@ fn use_map() -> Rc<RefCell<Option<Rc<Map>>>> {
         let map = map.clone();
         use_effect_with_deps(
             move |_| {
-                if let Ok(mut map) = map.try_borrow_mut() {
-                    map.replace(create_map());
-                } else {
-                    eprintln!("Failed to create Map");
-                }
+                let m = create_map();
+
+                let (tx, rx) = oneshot::channel();
+                let _ = m.on(Listener { tx: Some(tx) }).unwrap();
+
+                wasm_bindgen_futures::spawn_local(async move {
+                    rx.await.unwrap();
+                    info!("map loaded");
+                    if let Ok(mut map) = map.try_borrow_mut() {
+                        map.replace(m);
+                    } else {
+                        error!("Failed to create Map");
+                    }
+                });
                 || {}
             },
             (),
@@ -30,20 +77,25 @@ fn app() -> Html {
     let _map = use_map();
 
     html! {
-      <div id="map" style="width: 100vw; height: 100vh;"></div>
+      <div>
+        <div id="map" style="width: 100vw; height: 100vh;"></div>
+        <pre id="features"></pre>
+      </div>
     }
 }
 
 pub fn create_map() -> Rc<Map> {
-    let token = std::env!("MAPBOX_TOKEN");
+    let token = "pk.eyJ1IjoiZW56b2Zlcm5hbmRlczEyMyIsImEiOiJjbHh5M25nMmoyemJ1MnJwenJyaGdtczhvIn0.0BuZylH0eB-djtYNwyjW6w";
 
     let opts = MapOptions::new(token.into(), "map".into())
-        .center(LngLat::new(139.7647863, 35.6812373))
-        .zoom(15.0);
+        .center(LngLat::new(0.0,0.0))
+        //.style("mapbox://styles/mapbox/streets-v12".into())
+        .zoom(3.0);
 
     Map::new(opts).unwrap()
 }
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
 }
